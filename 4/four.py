cac6e4c5d8e78f1e5a6d5f2117b6971bde96ab19
bobby <bobbykushwah19082005@gmail.com>
2026-07-13 22:47:28 +0530

    final

---
+++ 4/four.py
@@ -0,0 +1,11 @@
+
+def pattern4(N):
+       
+        for i in range(1, N + 1):
+            for j in range(1, i + 1):
+                print(i, end=" ")
+            
+            print()
+
+N=int(input("enter your number"))
+pattern4(N)
